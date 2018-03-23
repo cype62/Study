68374db65b092556df02075bcf4a1714217943bf
陈耶鹏 <106834067@qq.com>
2018-03-23 22:43:48 +0800

    增加从控制台输入帐号密码，并传给userinfo类

--- test2/UserControl.m
+++ test2/UserControl.m
@@ -10,13 +10,28 @@
 #import "UserInfo.h"
 
 @implementation UserControl
+//登录方法
 -(void)loginMethod{
     
 }
 
+//注册方法
 -(void)registerMethod{
+    // 使用一个缓冲区
+    char buffer_name[1000];
+    char buffer_pw[1000];
+    // 读取控制台的输入信息
+    NSLog(@"请填写帐号：");
+    scanf("%s",buffer_name);
+    NSLog(@"\n请填写密码：");
+    scanf("%s",buffer_pw);
+    // 接收控制台输入信息（这里是以接收输入的序号为例）
+    NSString *name = [NSString stringWithUTF8String:buffer_name];
+    NSString *pw = [NSString stringWithUTF8String:buffer_pw];
     UserInfo *user = [[UserInfo alloc]init];
-    [user userDic:@"li" :@"123"];
+//    [user userDic:@"li" :@"123"];
+//    把账号密码传给user字典
+    [user userDic:name :pw];
     
 }
 @end
